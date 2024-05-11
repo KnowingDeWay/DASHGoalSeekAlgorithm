4985a40498e0da4c4cc6c6b28d83460773758772
Andre Bella <12886824@student.uts.edu.au>
2024-05-11 19:57:06 +1000

    Added commentary to back-end project

--- GoalSeekAlgorithm.Server/Controllers/ApiController.cs
+++ GoalSeekAlgorithm.Server/Controllers/ApiController.cs
@@ -1,12 +1,26 @@
 using GoalSeekAlgorithm.Server.RequestModels;
 using Microsoft.AspNetCore.Mvc;
+using MathNet.Numerics;
 
 namespace GoalSeekAlgorithm.Server.Controllers
 {
+    /// <summary>
+    /// Controller containing endpoint(s) that are involved in providing a suite of operations related with the usuage of the
+    /// Goal Seek Algorithm to seek solutions to various equations
+    /// </summary>
     [ApiController]
     [Route("[controller]")]
     public class ApiController : ControllerBase
     {
+        /// <summary>
+        /// An endpoint method which will attempt to find an approximate root/answer to a specified equation with parameters set
+        /// in the <see cref="GoalSeekRequestModel"/> object.
+        /// </summary>
+        /// <param name="requestModel">A request model containing parameters for executing the Goal Seek algorithm</param>
+        /// <returns>
+        /// A HTTP Response containing a response object with the answer and number of iterations executed to
+        /// reach the correct answer (if applicable)
+        /// </returns>
         [HttpPost(Name = "goalseek")]
         public ActionResult GoalSeek(GoalSeekRequestModel requestModel)
         {
@@ -18,7 +32,13 @@ namespace GoalSeekAlgorithm.Server.Controllers
             return Ok(answer);
         }
 
-        private int ApplyNewtonsMethod(int x)
+        /// <summary>
+        /// Calculates an approximate root using Newtown's Method
+        /// ( i.e. xn+1 = xn - f(xn)/f'(xn) )
+        /// </summary>
+        /// <param name="xn">Initial Guess</param>
+        /// <returns>A new approximation for the root of an equation (xn+1)</returns>
+        private int ApplyNewtonsMethod(int xn)
         {
             return 0;
         }
